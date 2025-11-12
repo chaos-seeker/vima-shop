d53548af6cd2f386679ba25de977ff672b17fce8
hamid shahsavani <051.sys113@gmail.com>
2025-11-12 09:50:26 +0330

    chore: new changes

--- containers/layout/dashboard/header.tsx
+++ containers/layout/dashboard/header.tsx
@@ -3,34 +3,51 @@
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { cn } from '@/utils/cn';
-import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
-import { RiArrowLeftLine, RiArrowRightLine, RiCloseLine, RiMenu4Fill } from 'react-icons/ri';
-import type { Swiper as SwiperType } from 'swiper';
-import { Navigation } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
+import {
+  RiAwardFill,
+  RiAwardLine,
+  RiCloseLine,
+  RiFolderFill,
+  RiFolderLine,
+  RiMenu4Fill,
+  RiMessageFill,
+  RiMessageLine,
+  RiShoppingBagFill,
+  RiShoppingBagLine,
+  RiShoppingCartFill,
+  RiShoppingCartLine,
+  RiUserFill,
+  RiUserLine,
+} from 'react-icons/ri';
 
 export const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header>
       <div className="container">
         <div className="relative">
           <div className="flex flex-col items-center justify-between bg-white border px-3.5 py-4 rounded-2xl my-5">
             <div className="flex w-full items-center justify-between">
-              <Link href="/">
-                <Image
-                  src="/images/layout/logo.png"
-                  alt="logo"
-                  width={110}
-                  height={100}
-                />
-              </Link>
-              <MobileHumbergerMenu />
-            </div>
-            <div className="mt-3 flex w-full justify-center border-t px-4 py-4">
-              <Tabs />
+              <Menu />
+              {pathname === '/dashboard/manage-products' && (
+                <button className="bg-primary text-white px-4 py-2.5 rounded-xl hover:bg-primary/80 transition-all">
+                  افزودن محصول
+                </button>
+              )}
+              {pathname === '/dashboard/manage-categories' && (
+                <button className="bg-primary text-white px-4 py-2.5 rounded-xl hover:bg-primary/80 transition-all">
+                  افزودن دسته‌بندی
+                </button>
+              )}
+              {pathname === '/dashboard/manage-brands' && (
+                <button className="bg-primary text-white px-4 py-2.5 rounded-xl hover:bg-primary/80 transition-all">
+                  افزودن برند
+                </button>
+              )}
             </div>
           </div>
         </div>
@@ -39,94 +56,85 @@ export const Header = () => {
   );
 };
 
-const MobileHumbergerMenu = () => {
+const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const data = [
+    {
+      label: 'مدیریت محصولات',
+      href: '/dashboard/manage-products',
+      icon: RiShoppingBagFill,
+      iconLine: RiShoppingBagLine,
+    },
+    {
+      label: 'مدیریت دسته‌بندی‌ها',
+      href: '/dashboard/manage-categories',
+      icon: RiFolderFill,
+      iconLine: RiFolderLine,
+    },
+    {
+      label: 'مدیریت برندها',
+      href: '/dashboard/manage-brands',
+      icon: RiAwardFill,
+      iconLine: RiAwardLine,
+    },
+    {
+      label: 'مدیریت سفارش‌ها',
+      href: '/dashboard/manage-orders',
+      icon: RiShoppingCartFill,
+      iconLine: RiShoppingCartLine,
+    },
+    {
+      label: 'مدیریت کاربران',
+      href: '/dashboard/manage-users',
+      icon: RiUserFill,
+      iconLine: RiUserLine,
+    },
+    {
+      label: 'مدیریت دیدگاه ها',
+      href: '/dashboard/manage-comments',
+      icon: RiMessageFill,
+      iconLine: RiMessageLine,
+    },
+  ];
+
   return (
-    <div className="lg:hidden flex items-center">
+    <div className="flex items-center">
       <button onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? (
-          <RiCloseLine className="text-black size-7" />
+          <RiCloseLine className="size-7" />
         ) : (
-          <RiMenu4Fill className="text-black size-7" />
+          <RiMenu4Fill className="size-7" />
         )}
       </button>
       <div
         className={cn(
-          'bg-white border px-3.5 py-4 rounded-2xl my-5 transition-all absolute w-full right-0 top-16',
+          'bg-white border px-3.5 py-4 w-fit rounded-2xl my-5 transition-all absolute right-0 top-16',
           isOpen ? 'opacity-100 visible' : 'opacity-0 invisible',
         )}
       >
         <ul>
-          <li>
-            <Link href="/" className="flex items-center gap-2">
-              <span>Home</span>
-            </Link>
-          </li>
+          {data.map((item) => {
+            const IconFill = item.icon;
+            const IconLine = item.iconLine;
+            return (
+              <li key={item.href}>
+                <Link
+                  href={item.href}
+                  className="flex items-center gap-2 rounded-lg px-3 py-2 transition-colors hover:bg-primary group"
+                >
+                  <span className="relative">
+                    <IconLine className="size-7 group-hover:opacity-0 group-hover:text-white transition-all" />
+                    <IconFill className="size-7 opacity-0 group-hover:opacity-100 group-hover:text-white transition-all absolute top-0 right-0" />
+                  </span>
+                  <span className="group-hover:text-white transition-colors">
+                    {item.label}
+                  </span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
-    </div>
-  );
-};
-
-const Tabs = () => {
-  const pathname = usePathname();
-  const activeTabRef = useRef<HTMLAnchorElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const data = [
-    { label: 'مدیریت محصولات', href: '/dashboard/manage-products' },
-    { label: 'مدیریت دسته‌بندی‌ها', href: '/dashboard/manage-categories' },
-    { label: 'مدیریت برندها', href: '/dashboard/manage-brands' },
-    { label: 'مدیریت سفارش‌ها', href: '/dashboard/manage-orders' },
-    { label: 'مدیریت کاربران', href: '/dashboard/manage-users' },
-    { label: 'مدیریت دیدگاه ها', href: '/dashboard/manage-comments' },
-  ];
-
-  useEffect(() => {
-    if (activeTabRef.current && containerRef.current) {
-      const container = containerRef.current;
-      const activeTab = activeTabRef.current;
-      const containerRect = container.getBoundingClientRect();
-      if (window.innerWidth < 1024) {
-        const tabRect = activeTab.getBoundingClientRect();
-        const scrollLeft =
-          activeTab.offsetLeft - containerRect.width / 2 + tabRect.width / 2;
-        container.scrollTo({
-          left: scrollLeft,
-          behavior: 'smooth',
-        });
-      }
-    }
-  }, [pathname]);
-
-  return (
-    <div
-      ref={containerRef}
-      className="flex snap-x snap-mandatory gap-4 overflow-x-auto scroll-smooth lg:justify-center lg:overflow-x-visible"
-      style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
-    >
-      {data.map((item) => {
-        const isActive =
-          pathname === item.href || pathname.startsWith(item.href + '/');
-        return (
-          <Link
-            ref={isActive ? activeTabRef : null}
-            href={item.href}
-            key={item.href}
-            className="shrink-0 snap-center"
-          >
-            <button
-              className={cn(
-                'rounded-xl px-4 py-2.5 text-sm font-medium',
-                isActive
-                  ? 'bg-primary text-white'
-                  : 'bg-transparent text-gray-500',
-              )}
-            >
-              {item.label}
-            </button>
-          </Link>
-        );
-      })}
     </div>
   );
 };
